8c6a80b8508ae7dcd52c4832fb4ea39e85533e68
Tosu <167279662+about-tosu@users.noreply.github.com>
2024-12-19 02:00:20 +0530

    Update privacy.py

--- AnonXMusic/plugins/bot/privacy.py
+++ AnonXMusic/plugins/bot/privacy.py
@@ -6,6 +6,10 @@ import config
 
 TEXT = f"""
 🔒 **Privacy Policy for Bot Hub Bot's !**
+
+Your privacy is important to us. To learn more about how we collect, use, and protect your data, please review our Privacy Policy here: [Privacy Policy](https://telegra.ph/Privacy-Policy-Bot-Hub-12-18-2).
+
+If you have any questions or concerns, feel free to reach out to our [support team](https://t.me/alice_x_support).
 """
 
 @app.on_message(filters.command("privacy"))
